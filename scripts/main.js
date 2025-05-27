46ac29c052929016a4b09d1f529b968fdea0224d
HoloProfile <holoprofile@pm.me>
2025-05-27 23:24:04 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -22,24 +22,4 @@ document.querySelectorAll('.accordion-header').forEach(btn => {
     content.classList.toggle('open');
 }
 
-// === dagens tip===
-document.addEventListener("DOMContentLoaded", function () {
-  const tips = [
-    "Gem dit kogevand – dine planter vil elske det!",
-    "Drop bilen - Tag cyklen – også selvom det regner 🚲",
-    "Brug den samme pose flere gange, evt. stof – plastik er last year's problem ♻️",
-    "Gå offline en time – og se hvad du finder ☀️",
-    "Lav aftensmad af resterne 🥦",
-    "Del din viden med en ven i dag 💬",
-    "Reparer noget, bare én ting 🔧",
-    "Genbrug et glas som opbevaring 🫙",
-    "Lad op med solens energi – også mentalt ☀️",
-    "Spis kødfrit 3 dage denne uge"
-  ];
-
-  const tipEl = document.getElementById("planetTip");
-  if (tipEl) {
-    const randomTip = tips[Math.floor(Math.random() * tips.length)];
-    tipEl.textContent = `🌾 Dagens planetære tip: ${randomTip}`;
-  }
 });
